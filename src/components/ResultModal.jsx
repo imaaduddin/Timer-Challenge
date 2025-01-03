cc23403b5cf4ecc8dbb3fae135a7f115939f34e5
Imaad Uddin <imaaduddin27@gmail.com>
2025-01-02 19:31:40 -0600

    working on start and stop timer

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,10 +1,25 @@
-export default function ResultModal(result, targetTime) {
+import { useImperativeHandle, useRef } from "react";
+
+export default function ResultModal({ ref, targetTime, remainingTime, onReset }) {
+    const dialog = useRef();
+
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialog.current.showModal();
+            }
+        };
+    });
+
     return (
-        <dialog className="result-modal">
-            <h2>You {result}</h2>
+        <dialog ref={dialog} className="result-modal">
+            {userLost && <h2>You lost.</h2>}
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
-            <p>You stopped the timer with <strong>X seconds left.</strong></p>
-            <form method="dialog">
+            <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+            <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
